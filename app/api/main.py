8fdbcf15dc7a7faf3b8f341ddc98efb1f264afa0
rencoryf <yaroslavdotsenko10@gmail.com>
2025-12-08 08:02:32 +0300

    add reg endpoint

--- app/api/main.py
+++ app/api/main.py
@@ -1,16 +1,3 @@
 from fastapi import FastAPI, Request
 
-from app.models.api.api_verify_class import ApiVerifyClass
-
 app = FastAPI()
-
-
-@app.get("/user")
-async def get_user(request: Request):
-    auth_header = request.headers.get("Authorization")
-    if auth_header:
-        token = auth_header.split(" ")[1]
-        user_id = ApiVerifyClass.verify_token(token)
-        if user_id:
-            return {"id": user_id}
-    return {"error": "Unauthorized"}
